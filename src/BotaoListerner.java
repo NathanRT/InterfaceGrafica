7c6bddf92b51febe6dccadfc1b6a37b4ddfd2cee
NathanRT <82781967+NathanRT@users.noreply.github.com>
2021-04-19 22:05:00 -0300

    componentes de intrface grafica

--- src/BotaoListerner.java
+++ src/BotaoListerner.java
@@ -1,8 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -41,10 +39,15 @@ public class BotaoListener extends JFrame implements ActionListener {
 		
 	public void actionPerformed(ActionEvent e) {
 		
+		Filme filme = new Filme();
+		filme.setTitulo(textTitulo.getText());
+		filme.setSinopse(textSinopse.getText());
+		filme.setGenero((String) cnbGenero.getSelectedItem());		
+		filme.setavaliacao(StarRater.getSelection());
+		
 		if(assistido.isSelected()) {
 			assistir = "Sim";
-		}
-		else {
+		} else {
 			assistir = "Não";
 		}
 		
@@ -61,16 +64,16 @@ public class BotaoListener extends JFrame implements ActionListener {
 		if(textTitulo.getText().equals("") || cnbGenero.getSelectedItem() == null){
 			JOptionPane.showMessageDialog(null, "Por favor, preecha todos os campos!");
 		} else {
-			JOptionPane.showMessageDialog(null, 
-				"* FILME *\n" + 
-				"-  " + textTitulo.getText() + "\n" +
-				"-  " + textSinopse.getText() + "\n " +
-				"-  " + cnbGenero.getSelectedItem() + "\n " +
-				"-  " + Canal + "\n " +
-				"-  " + assistir + "\n " +
-				"-  " + StarRater.getSelection() + " Estrelas"
+		JOptionPane.showMessageDialog(null, 
+				
+				"Titulo: " + filme.getTitulo()
+				+ "\nSinopse: " + filme.getSinopse()
+				+ "\nGenero: " + filme.getGenero()
+				+ "\nOnde assistir: " + Canal 
+				+ "\nAssistido: " + assistir
+				+ "\nAvaliaçãor: " + filme.getavaliacao()
 				);
-		}
+		}	
 
 	}
 }
